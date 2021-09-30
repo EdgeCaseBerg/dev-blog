4388314a22a107054ced74a1f056a0e3d0aac25b
Ethan Eldridge <ejayeldridge@gmail.com>
2021-09-30 03:12:52 -0400

    Simplify Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <b-container>
-    <h1>Hi</h1>
+    <b-row>
+      <b-col>
+        <h1>Welcome</h1>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <p>
+          There's not much to say here. This site is just a collection of thoughts about development really, so you probably want to click on the <router-link to="/dev-blog">blog link.</router-link>
+        </p>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
-
-<script>
-export default {
-  name: "home",
-};
-</script>
-<style scoped></style>
